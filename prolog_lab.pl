ee94a2b1f2ed1cba95f923eada5012eced694722
Kyle Webster <kyle1234webster@gmail.com>
2018-04-16 10:41:59 -0600

    Finished

--- prolog_lab.pl
+++ prolog_lab.pl
@@ -25,3 +25,29 @@ sister(S,X):- female(S), sibling(S,X).
 uncle(U,C):- brother(U,X), parent(X, C).
 % A married uncle U is male, spouse of child C's parent Z's sibling X
 uncle(U,C):- male(U), spouse(U,X), sibling(X,Z), parent(Z,C).
+% A blood aunt A is a sister of child C's parent X
+aunt(A,C):- sister(A,X), parent(X,C).
+% A married aunt A is a female, a spouse of child C's parent Z's sibling X
+aunt(A,C):- female(A), spouse(A,X), sibling(X,Z), parent(Z,C).
+% A grandparent G is the parent of X who where X is the parent of child C
+grandparent(G,C):- parent(G,X), parent(X,C).
+% A grandfather F of child C is C's grandparent and male
+grandfather(F,C):- grandparent(F,C), male(F).
+% A grandfather M of child C is C's grandparent and female
+grandmother(M,C):- grandparent(M,C), female(M).
+% A grandchild C has a grandparent G and Parent X
+grandchild(C,G):- grandparent(G,X), parent(X,C).
+% A person X is an Ancestor of Y
+ancestor(X,Y):- parent(X,Y).
+ancestor(X,Y):- parent(X,Z), ancestor(Z,Y).
+% A person X is a decendent of Y
+decendent(X,Y):- parent(Y,X).
+decendent(X,Y):- parent(Z,X), decendent(Z,Y).
+% determines if X is older than Y
+older(X,Y):- born(X,A), born(Y,B), A>B.
+% determines if X is younger than Y
+younger(X,Y):- born(X,A), born(Y,B), B>A.
+% determines if X was ruling when Y was born. S is when Reign started and F is when it finished
+regentWhenBorn(X,Y):- reigned(X,S,F), born(Y,A), A>S, F>A.
+% Cousins X and Y has parents who are siblings
+cousin(X,Y):- parent(AU, X), parent(Z,Y), sibling(AU,Z).
